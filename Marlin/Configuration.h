2e681aaee67df243c9ca5392a57afcf97e2a3d51
bdougs <67288915+bdougs@users.noreply.github.com>
2020-10-22 10:32:43 -0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "BS_10-20-20" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "BS_10-22-20" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "CE5SKRTurbo"
+#define CUSTOM_MACHINE_NAME "CE5SKR1.4Turbo"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
