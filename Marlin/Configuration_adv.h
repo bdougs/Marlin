7e501595c37834de3f20c22e57ce818785ef4663
bdougs <67288915+bdougs@users.noreply.github.com>
2021-02-24 07:47:29 -0800

    updates

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2220,7 +2220,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       900        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  400  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  300  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -2503,9 +2503,9 @@
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
-    #define X_STALL_SENSITIVITY  102
+    #define X_STALL_SENSITIVITY  112
     //#define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
-    #define Y_STALL_SENSITIVITY  105
+    #define Y_STALL_SENSITIVITY  102
     //#define Y2_STALL_SENSITIVITY Y_STALL_SENSITIVITY
     //#define Z_STALL_SENSITIVITY  8
     //#define Z2_STALL_SENSITIVITY Z_STALL_SENSITIVITY
